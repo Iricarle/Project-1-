d9592fc336bdbc0db3c3f71f3e269a41df157a2d
Iricarle <121541871+Iricarle@users.noreply.github.com>
2023-02-10 10:28:22 +0300

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,15 @@
+--/****** Script for SelectTopNRows command from SSMS  ******/
+--SELECT TOP (1000) [EmployeeID]
+--      ,[FirstName]
+--      ,[LastName]
+--      ,[Age]
+--      ,[Gender]
+--  FROM [SQL CLASSES].[dbo].[Employeesdemographics]
+
+  --Select * 
+  --from Employeesdemographics
+
+  --Select * 
+  --from [SQL CLASSES].[dbo].[Employeesdemographics]
+
+  
